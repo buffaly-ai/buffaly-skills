cc6782a84626bbc5a9a6aa806518605c6a8c48e8
Matt Furnari <mattfurnari@hotmail.com>
2026-09-04 12:48:38 +0000

    Publish extension Buffaly.Agent.Wiki

--- web-modules/Buffaly.Agent.Wiki/Scripts/Sessions_GetSidebarRootPageSp.sql
+++ web-modules/Buffaly.Agent.Wiki/Scripts/Sessions_GetSidebarRootPageSp.sql
@@ -23,18 +23,42 @@ AS
 
 	IF ISNULL(@Search, '') <> ''
 	BEGIN
-		;WITH MatchingSessions AS
+		;WITH SearchableSessions AS
+		(
+			SELECT	sessionRow.*
+			FROM	dbo.Sessions sessionRow WITH (NOLOCK)
+			WHERE	ISNULL(sessionRow.IsArchived, 0) = 0
+					AND sessionRow.SessionKey NOT IN (N'Browser Profiles', N'browser-profiles')
+		),
+		MatchingSessions AS
 		(
 			SELECT	sessionRow.SessionID,
 					sessionRow.ParentSessionID,
 					sessionRow.LastUpdated
-			FROM	dbo.Sessions sessionRow WITH (NOLOCK)
-			WHERE	ISNULL(sessionRow.IsArchived, 0) = 0
-					AND
-					(
-						sessionRow.SessionKey LIKE @SearchPattern
-						OR sessionRow.SessionName LIKE @SearchPattern
-					)
+			FROM	SearchableSessions sessionRow
+			WHERE	sessionRow.SessionKey LIKE @SearchPattern
+					OR sessionRow.SessionName LIKE @SearchPattern
+		),
+		CappedMatchingSessions AS
+		(
+			SELECT TOP (201)
+					matching.SessionID,
+					matching.ParentSessionID,
+					matching.LastUpdated
+			FROM	MatchingSessions matching
+			ORDER BY matching.LastUpdated DESC,
+					matching.SessionID DESC
+		),
+		WalkedMatchingSessions AS
+		(
+			SELECT TOP (200)
+					matching.SessionID,
+					matching.ParentSessionID,
+					matching.LastUpdated,
+					CONVERT(int, CASE WHEN (SELECT COUNT(*) FROM CappedMatchingSessions) > 200 THEN 1 ELSE 0 END) AS IsMatchSetBounded
+			FROM	CappedMatchingSessions matching
+			ORDER BY matching.LastUpdated DESC,
+					matching.SessionID DESC
 		),
 		MatchAncestors AS
 		(
@@ -43,7 +67,7 @@ AS
 					matching.SessionID AS MatchSessionID,
 					matching.LastUpdated,
 					1 AS DistanceFromMatch
-			FROM	MatchingSessions matching
+			FROM	WalkedMatchingSessions matching
 
 			UNION ALL
 
@@ -53,9 +77,8 @@ AS
 					child.LastUpdated,
 					child.DistanceFromMatch + 1
 			FROM	MatchAncestors child
-			JOIN	dbo.Sessions parent WITH (NOLOCK)
+			JOIN	SearchableSessions parent
 			ON		parent.SessionID = child.ParentSessionID
-					AND ISNULL(parent.IsArchived, 0) = 0
 		),
 		MatchingRows AS
 		(
@@ -63,8 +86,9 @@ AS
 					matching.ParentSessionID,
 					root.SessionID AS RootSessionID,
 					root.DistanceFromMatch AS HierarchyDepth,
-					matching.LastUpdated
-			FROM	MatchingSessions matching
+					matching.LastUpdated,
+					matching.IsMatchSetBounded
+			FROM	WalkedMatchingSessions matching
 			JOIN	MatchAncestors root
 			ON		root.MatchSessionID = matching.SessionID
 					AND root.ParentSessionID IS NULL
@@ -81,6 +105,7 @@ AS
 		PagedRoots AS
 		(
 			SELECT	RootSessionID,
+					EffectiveLastUpdated,
 					RootOrdinal,
 					TotalRootRows,
 					COUNT(*) OVER () AS RootRowsReturned
@@ -94,9 +119,13 @@ AS
 					matching.RootSessionID,
 					matching.HierarchyDepth,
 					matching.LastUpdated,
+					pagedRoot.EffectiveLastUpdated AS RootEffectiveLastUpdated,
 					pagedRoot.RootOrdinal,
 					pagedRoot.TotalRootRows,
 					pagedRoot.RootRowsReturned,
+					matching.IsMatchSetBounded,
+					COUNT(*) OVER (PARTITION BY matching.RootSessionID) AS MatchingRowsWithinRoot,
+					COUNT(*) OVER () AS TotalMatchingRows,
 					ROW_NUMBER() OVER
 					(
 						PARTITION BY matching.RootSessionID
@@ -114,8 +143,13 @@ AS
 		),
 		CappedMatchingRows AS
 		(
-			SELECT	*
-			FROM	RankedMatchingRows
+			SELECT	*,
+					CONVERT(int, CASE
+						WHEN MAX(matching.IsMatchSetBounded) OVER () = 1
+							OR TotalMatchingRows > 200
+							OR MatchingRowsWithinRoot > CONVERT(int, CEILING(200.0 / NULLIF(RootRowsReturned, 0)))
+						THEN 1 ELSE 0 END) AS IsSearchBounded
+			FROM	RankedMatchingRows matching
 			WHERE	SearchResultOrdinalWithinRoot <= CONVERT(int, CEILING(200.0 / NULLIF(RootRowsReturned, 0)))
 					AND SearchResultOrdinal <= 200
 		),
@@ -126,10 +160,12 @@ AS
 					matching.RootSessionID,
 					matching.HierarchyDepth,
 					matching.LastUpdated,
+					matching.RootEffectiveLastUpdated,
 					matching.RootOrdinal,
 					matching.TotalRootRows,
 					matching.RootRowsReturned,
 					matching.SearchResultOrdinal,
+					matching.IsSearchBounded,
 					CONVERT(bit, 1) AS IsSearchMatch
 			FROM	CappedMatchingRows matching
 
@@ -140,15 +176,16 @@ AS
 					child.RootSessionID,
 					child.HierarchyDepth - 1,
 					parent.LastUpdated,
+					child.RootEffectiveLastUpdated,
 					child.RootOrdinal,
 					child.TotalRootRows,
 					child.RootRowsReturned,
 					child.SearchResultOrdinal,
+					child.IsSearchBounded,
 					CONVERT(bit, 0) AS IsSearchMatch
 			FROM	SearchRowsWithAncestors child
-			JOIN	dbo.Sessions parent WITH (NOLOCK)
+			JOIN	SearchableSessions parent
 			ON		parent.SessionID = child.ParentSessionID
-					AND ISNULL(parent.IsArchived, 0) = 0
 		),
 		DedupedSearchRows AS
 		(
@@ -156,18 +193,21 @@ AS
 					searchRow.RootSessionID,
 					MIN(searchRow.HierarchyDepth) AS HierarchyDepth,
 					MAX(searchRow.LastUpdated) AS LastUpdated,
+					MAX(searchRow.RootEffectiveLastUpdated) AS RootEffectiveLastUpdated,
 					MIN(searchRow.RootOrdinal) AS RootOrdinal,
 					MAX(searchRow.TotalRootRows) AS TotalRootRows,
 					MAX(searchRow.RootRowsReturned) AS RootRowsReturned,
 					MIN(searchRow.SearchResultOrdinal) AS SearchResultOrdinal,
+					MAX(searchRow.IsSearchBounded) AS IsSearchBounded,
 					MAX(CONVERT(int, searchRow.IsSearchMatch)) AS IsSearchMatch
 			FROM	SearchRowsWithAncestors searchRow
 			GROUP BY searchRow.SessionID,
-					searchRow.RootSessionID
+					 searchRow.RootSessionID
 		)
 		SELECT	sessionRow.SessionID,
 				sessionRow.SessionKey,
 				sessionRow.ParentSessionID,
+				parent.SessionKey AS ParentSessionKey,
 				sessionRow.SessionName,
 				sessionRow.AgentName,
 				sessionRow.ProjectName,
@@ -175,20 +215,23 @@ AS
 				sessionRow.Provider,
 				sessionRow.ModelName,
 				sessionRow.ReasoningLevel,
-				sessionRow.PromptContext,
+				CONVERT(nvarchar(max), N'') AS PromptContext,
+				sessionRow.CompactionProvider,
 				sessionRow.Data,
 				sessionRow.DateCreated,
 				sessionRow.LastUpdated AS OwnLastUpdated,
-				sessionRow.LastUpdated AS EffectiveLastUpdated,
-				sessionRow.IsArchived,
+				CASE WHEN deduped.HierarchyDepth = 1 THEN deduped.RootEffectiveLastUpdated ELSE sessionRow.LastUpdated END AS EffectiveLastUpdated,
 				deduped.RootSessionID,
 				CONVERT(int, deduped.RootOrdinal) AS RootOrdinal,
 				deduped.HierarchyDepth,
 				CONVERT(int, deduped.RootRowsReturned) AS RootRowsReturned,
-				CONVERT(bit, CASE WHEN deduped.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows
+				CONVERT(bit, CASE WHEN deduped.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows,
+				CONVERT(bit, MAX(deduped.IsSearchBounded) OVER ()) AS IsSearchBounded
 		FROM	DedupedSearchRows deduped
-		JOIN	dbo.Sessions sessionRow WITH (NOLOCK)
+		JOIN	SearchableSessions sessionRow
 		ON		sessionRow.SessionID = deduped.SessionID
+		LEFT JOIN SearchableSessions parent
+		ON		parent.SessionID = sessionRow.ParentSessionID
 		ORDER BY deduped.RootOrdinal,
 				deduped.HierarchyDepth,
 				deduped.SearchResultOrdinal,
@@ -200,36 +243,19 @@ AS
 
 	;WITH RootActivity AS
 	(
-		SELECT		root.SessionID AS RootSessionID,
-					CASE
-						WHEN MAX(child.LastUpdated) IS NOT NULL AND MAX(child.LastUpdated) > root.LastUpdated THEN MAX(child.LastUpdated)
-						ELSE root.LastUpdated
-					END AS EffectiveLastUpdated
-		FROM		dbo.Sessions root WITH (NOLOCK)
-		LEFT JOIN	dbo.Sessions child WITH (NOLOCK)
-		ON			child.ParentSessionID = root.SessionID
-					AND ISNULL(child.IsArchived, 0) = 0
-		WHERE		root.ParentSessionID IS NULL
-					AND ISNULL(root.IsArchived, 0) = 0
-					AND
-					(
-						root.SessionKey LIKE @SearchPattern
-						OR root.SessionName LIKE @SearchPattern
-						OR EXISTS
-						(
-							SELECT 1
-							FROM dbo.Sessions matchingChild WITH (NOLOCK)
-							WHERE matchingChild.ParentSessionID = root.SessionID
-								AND ISNULL(matchingChild.IsArchived, 0) = 0
-								AND
-								(
-									matchingChild.SessionKey LIKE @SearchPattern
-									OR matchingChild.SessionName LIKE @SearchPattern
-								)
-						)
-					)
-		GROUP BY	root.SessionID,
-					root.LastUpdated
+		SELECT	root.SessionID AS RootSessionID,
+				CASE
+					WHEN MAX(child.LastUpdated) IS NOT NULL AND MAX(child.LastUpdated) > root.LastUpdated THEN MAX(child.LastUpdated)
+					ELSE root.LastUpdated
+				END AS EffectiveLastUpdated
+		FROM	dbo.Sessions root WITH (NOLOCK)
+		LEFT JOIN dbo.Sessions child WITH (NOLOCK)
+		ON		child.ParentSessionID = root.SessionID
+				AND ISNULL(child.IsArchived, 0) = 0
+		WHERE	root.ParentSessionID IS NULL
+				AND ISNULL(root.IsArchived, 0) = 0
+		GROUP BY root.SessionID,
+				 root.LastUpdated
 	),
 	RankedRoots AS
 	(
@@ -254,6 +280,7 @@ AS
 		SELECT	root.SessionID,
 				root.SessionKey,
 				root.ParentSessionID,
+				CONVERT(nvarchar(255), NULL) AS ParentSessionKey,
 				root.SessionName,
 				root.AgentName,
 				root.ProjectName,
@@ -261,17 +288,18 @@ AS
 				root.Provider,
 				root.ModelName,
 				root.ReasoningLevel,
-				root.PromptContext,
+				CONVERT(nvarchar(max), N'') AS PromptContext,
+				root.CompactionProvider,
 				root.Data,
 				root.DateCreated,
 				root.LastUpdated AS OwnLastUpdated,
 				pagedRoot.EffectiveLastUpdated,
-				root.IsArchived,
 				pagedRoot.RootSessionID,
 				CONVERT(int, pagedRoot.RootOrdinal) AS RootOrdinal,
 				1 AS HierarchyDepth,
 				CONVERT(int, pagedRoot.RootRowsReturned) AS RootRowsReturned,
-				CONVERT(bit, CASE WHEN pagedRoot.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows
+				CONVERT(bit, CASE WHEN pagedRoot.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows,
+				CONVERT(bit, 0) AS IsSearchBounded
 		FROM	PagedRoots pagedRoot
 		JOIN	dbo.Sessions root WITH (NOLOCK)
 		ON		root.SessionID = pagedRoot.RootSessionID
@@ -281,6 +309,7 @@ AS
 		SELECT	child.SessionID,
 				child.SessionKey,
 				child.ParentSessionID,
+				parent.SessionKey AS ParentSessionKey,
 				child.SessionName,
 				child.AgentName,
 				child.ProjectName,
@@ -288,31 +317,29 @@ AS
 				child.Provider,
 				child.ModelName,
 				child.ReasoningLevel,
-				child.PromptContext,
+				CONVERT(nvarchar(max), N'') AS PromptContext,
+				child.CompactionProvider,
 				child.Data,
 				child.DateCreated,
 				child.LastUpdated AS OwnLastUpdated,
 				child.LastUpdated AS EffectiveLastUpdated,
-				child.IsArchived,
 				pagedRoot.RootSessionID,
 				CONVERT(int, pagedRoot.RootOrdinal) AS RootOrdinal,
 				2 AS HierarchyDepth,
 				CONVERT(int, pagedRoot.RootRowsReturned) AS RootRowsReturned,
-				CONVERT(bit, CASE WHEN pagedRoot.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows
+				CONVERT(bit, CASE WHEN pagedRoot.TotalRootRows > @SkipRoots + @NumRoots THEN 1 ELSE 0 END) AS HasMoreRootRows,
+				CONVERT(bit, 0) AS IsSearchBounded
 		FROM	PagedRoots pagedRoot
+		JOIN	dbo.Sessions parent WITH (NOLOCK)
+		ON		parent.SessionID = pagedRoot.RootSessionID
 		JOIN	dbo.Sessions child WITH (NOLOCK)
 		ON		child.ParentSessionID = pagedRoot.RootSessionID
 				AND ISNULL(child.IsArchived, 0) = 0
-				AND
-				(
-					ISNULL(@Search, '') = ''
-					OR child.SessionKey LIKE @SearchPattern
-					OR child.SessionName LIKE @SearchPattern
-				)
 	)
 	SELECT	SessionID,
 			SessionKey,
 			ParentSessionID,
+			ParentSessionKey,
 			SessionName,
 			AgentName,
 			ProjectName,
@@ -321,19 +348,21 @@ AS
 			ModelName,
 			ReasoningLevel,
 			PromptContext,
+			CompactionProvider,
 			Data,
 			DateCreated,
 			OwnLastUpdated,
 			EffectiveLastUpdated,
-			IsArchived,
 			RootSessionID,
 			RootOrdinal,
 			HierarchyDepth,
 			RootRowsReturned,
-			HasMoreRootRows
+			HasMoreRootRows,
+			IsSearchBounded
 	FROM	SidebarRows
 	ORDER BY RootOrdinal,
 			HierarchyDepth,
 			OwnLastUpdated DESC,
 			SessionID DESC
+	OPTION (MAXDOP 1);
 GO
