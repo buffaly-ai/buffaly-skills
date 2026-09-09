cd5d49b9a3026463fae85946b842e32a08e0b54e
Matt Furnari <mattfurnari@hotmail.com>
2026-09-09 10:19:25 +0000

    Publish extension Sessions

---
+++ web-modules/Sessions/Scripts/TryUpdateFeatureSettingsSp.sql
@@ -0,0 +1,22 @@
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE Specific_Name = 'TryUpdateFeatureSettingsSp')
+BEGIN
+    DROP PROCEDURE TryUpdateFeatureSettingsSp
+END
+GO
+
+CREATE PROCEDURE dbo.TryUpdateFeatureSettingsSp (
+    @FeatureID int,
+    @ExpectedSettings nvarchar(max),
+    @Settings nvarchar(max)
+)
+AS
+    SET NOCOUNT ON
+
+    UPDATE Features
+    SET Settings = @Settings,
+        LastUpdated = GETDATE()
+    WHERE FeatureID = @FeatureID
+      AND ((Settings = @ExpectedSettings) OR (Settings IS NULL AND @ExpectedSettings IS NULL))
+
+    SELECT CONVERT(int, @@ROWCOUNT) AS UpdatedRows
+GO
